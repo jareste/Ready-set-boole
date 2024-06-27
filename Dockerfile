6435f422a01c0259d2546c0fb232089e5a200198
Jareste- <124678884+jareste@users.noreply.github.com>
2024-06-27 15:11:21 +0000

    negation_normal_form done

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,8 @@ RUN rustc multiplier.rs -o multiplier
 RUN rustc gray_code.rs -o gray_code
 RUN rustc evaluation.rs -o evaluation
 RUN rustc truth_table.rs -o truth_table
-# RUN rustc negation_normal_form.rs -o negation_normal_form NOOK
+RUN rustc negation_normal_form.rs -o negation_normal_form
 RUN rustc sat.rs -o sat
 
-CMD ["sh", "-c", "./adder && ./multiplier && ./gray_code && ./evaluation && ./truth_table && ./sat"]
+CMD ["sh", "-c", "./adder && ./multiplier && ./gray_code && ./evaluation && ./truth_table && ./negation_normal_form && ./sat"]
 
--- negation_normal_form.rs
+++ negation_normal_form.rs
@@ -1,26 +1,158 @@
-fn negation_normal_form(formula: &str) -> String {
-    let mut stack = Vec::new();
-    for c in formula.chars() {
-        match c {
-            '0' | '1' | 'A'..='Z' => stack.push(c.to_string()),
-            '!' => {
-                let a = stack.pop().unwrap();
-                stack.push(format!("{}!", a));
-            }
-            '&' | '|' => {
-                let b = stack.pop().unwrap();
-                let a = stack.pop().unwrap();
-                stack.push(format!("{}{}{}", a, b, c));
-            }
-            _ => panic!("Invalid character in formula"),
-        }
-    }
-    stack.pop().unwrap()
+#[derive(Debug)]
+struct Params
+{
+    left: String,
+    right: String,
 }
 
-fn main() {
-    println!("######### NEGATION NORM FORM #########");
-    println!("{}", negation_normal_form("AB|!")); // A!B!|
-    println!("######### NEGATION NORM FORM #########");
+fn get_params(formula: &str, pos: usize) -> Params
+{
+    let mut stack = Vec::new();
+    for c in formula[..pos].chars()
+    {
+        stack.push(c.to_string());
+    }
+    let right = stack.pop().unwrap();
+    let left = stack.pop().unwrap();
+    Params { left, right }
+}
+
+fn rm_double_negations(s: &str) -> String
+{
+    let mut r = String::new();
+    let mut should_pass = false;
+    for i in 0..s.len()
+    {
+        if should_pass
+        {
+            should_pass = false;
+            continue;
+        }
+        if s.chars().nth(i).unwrap() != '!'
+        {
+            r.push(s.chars().nth(i).unwrap());
+            continue;
+        }
+        if i + 1 == s.len() || s.chars().nth(i + 1).unwrap() != '!'
+        {
+            r.push(s.chars().nth(i).unwrap());
+            continue;
+        }
+        should_pass = true;
+    }
+    r
+}
+
+fn rm_material_conditions(s: &str) -> String
+{
+    let mut r = String::new();
+    for i in 0..s.len() {
+        if s.chars().nth(i).unwrap() != '>'
+        {
+            r.push(s.chars().nth(i).unwrap());
+            continue;
+        }
+        let ps = get_params(&(r.clone() + ">"), r.len());
+        let a = ps.left;
+        let b = ps.right;
+        r = r[..r.len() - b.len()].to_string();
+        r.push_str(&format!("!{}|", b));
+    }
+    r
+}
+
+fn rm_equivalence(s: &str) -> String
+{
+    let mut r = String::new();
+    for i in 0..s.len()
+    {
+        if s.chars().nth(i).unwrap() != '='
+        {
+            r.push(s.chars().nth(i).unwrap());
+            continue;
+        }
+        let ps = get_params(&(r.clone() + "="), r.len());
+        let a = ps.left;
+        let b = ps.right;
+        r.push_str(&format!(">{}{}>&", b, a));
+    }
+    r
+}
+
+fn rm_xor(s: &str) -> String {
+    let mut r = String::new();
+    for i in 0..s.len()
+    {
+        if s.chars().nth(i).unwrap() != '^'
+        {
+            r.push(s.chars().nth(i).unwrap());
+            continue;
+        }
+        let ps = get_params(&(r.clone() + "^"), r.len());
+        let a = ps.left;
+        let b = ps.right;
+        r.push_str(&format!("!&{}{}!&|", b, a));
+    }
+    r
+}
+
+fn apply_morgan(s: &str) -> String
+{
+    let mut r = String::new();
+    let mut should_skip = false;
+    for i in 0..s.len()
+    {
+        if should_skip
+        {
+            should_skip = false;
+            continue;
+        }
+        if i == s.len() - 1 || !((s.chars().nth(i + 1).unwrap() == '!') && (s.chars().nth(i).unwrap() == '&' || s.chars().nth(i).unwrap() == '|'))
+        {
+            r.push(s.chars().nth(i).unwrap());
+            continue;
+        }
+        let ps = get_params(&(r.clone() + "&"), r.len());
+        let a = ps.left;
+        let b = ps.right;
+        r = r[..r.len() - b.len()].to_string();
+        r.push_str(&format!("!{}!{}", b, if s.chars().nth(i).unwrap() == '|' { "&" } else { "|" }));
+        should_skip = true;
+    }
+    r
+}
+
+fn is_valid(s: &str) -> bool
+{
+    !(s.contains("!!") || s.contains("&!") || s.contains("|!") || s.contains("=") || s.contains("^") || s.contains(">"))
+}
+
+fn negation_normal_form(formula: &str) -> String
+{
+    let mut s = formula.to_string();
+    loop
+    {
+        s = rm_xor(&s);
+        s = rm_equivalence(&s);
+        s = rm_material_conditions(&s);
+        s = apply_morgan(&s);
+        s = rm_double_negations(&s);
+        if is_valid(&s)
+        {
+            break;
+        }
+    }
+    s
+}
+
+fn main()
+{
+    println!("######### NEGATION NORMAL FORM #########");
+    println!("{} || A!B!|", negation_normal_form("AB&!"));
+    println!("{} || A!B!&", negation_normal_form("AB|!"));
+    println!("{} || A!B|", negation_normal_form("AB>"));
+    println!("{} || AB&A!B!&|", negation_normal_form("AB="));
+    println!("{} || A!B!&C!|", negation_normal_form("AB|C&!")); 
+    println!("######### NEGATION NORMAL FORM #########");
     println!("");
 }
